368251e9e2bd447d03fc92ff074240034ecabd6d
Oringes9235 <xfp20080128xfp@163.com>
2022-10-22 22:25:22 +0800

    [update]: renewable kernel compiler scripts

--- Kernel/Compiler.sh
+++ Kernel/Compiler.sh
@@ -1,6 +1,6 @@
 mkdir bin/
 
-clang KernelMaster.c Memory.c Shell.c Video.c -c -nostdlib -ffreestanding
+clang KernelMaster.c ./Power/Memory.c ./Shell/Shell.c ./Power/Video.c -c -nostdlib -ffreestanding
 
 ld KernelMaster.o Memory.o Shell.o Video.o -e KernelStart -o ./bin/Kernel.elf
 
